542c04b9e8611e96b01dc0e9c9668a7f3b2ad285
unidevGO_QA <symoonrezapatwaryanik@gmail.com>
2024-03-06 04:41:21 +0600

    Updated Codes

--- baseConfig.js
+++ baseConfig.js
@@ -2,15 +2,15 @@ var date = new Date();
 var vccd = date.getFullYear() + ("0" + (date.getMonth() + 1)).slice(-2) + ("0" + date.getDate()).slice(-2) + ("0" + date.getHours()).slice(-2) + ("0" + date.getMinutes()).slice(-2) + ("0" + date.getSeconds()).slice(-2);
 const testConfig =
 {
-      "Project": "FanSee UI Automation",
+      "Project": "Automation Exercise UI Automation",
       "Env":"Main",
       "Language":"",
       "elementFinderURL":"https://colorlib.com/etc/regform/colorlib-regform-36/",
       //Mail COnfigurations
-      "FromMailaddress": "udgo.sabbir@gmail.com",
+      "FromMailaddress": "symoon.reza@unidevgo.com",
       "FromAddressPassword": "jwqtedxspnbbdknu",
-      "ToMailAddress": ["sabbir.of@gmail.com"],
-      "TeamsWebhookURL": "https://transportedlabs.webhook.office.com/webhookb2/1af1ca62-123b-4948-9a2f-f84e4cb589e6@fee95589-ad46-417d-9636-8b4078359ad9/IncomingWebhook/dbccaaa1799743e38ed4d57ba2b562bc/a6b92662-3ec5-443a-8d4e-10db06594a3c",
+      "ToMailAddress": ["symoon.reza@unidevgo.com"],
+      "TeamsWebhookURL": "",
       "MailServiceProvider": "Gmail",
       "MailNotification": "yes",
       "TeamsNotification": "yes",
--- fixtures/basePages.ts
+++ fixtures/basePages.ts
@@ -1,6 +1,5 @@
 
 import { test as baseTest } from "@playwright/test";
-import functions from "@helper-function/functions";
 import BaseFunctions from "@base-function/baseFunctions";
 import testData from "@testData/testData"
 
@@ -9,15 +8,13 @@ import AutomationExPage from "@pages/AutomationEx.page";
 
 
 const test = baseTest.extend<{
-    functions: functions;
+    
     testData: testData;
     BaseFunctions: BaseFunctions;
     AutomationExPage:AutomationExPage;
 
 }>({
-    functions: async ({ page }, use) => {
-        await use(new functions(page));
-    },
+    
     testData: async ({ page }, use) => {
         await use(new testData(page));
     },
